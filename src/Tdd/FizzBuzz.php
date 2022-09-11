f16df772f9629fa5402d4bb799d89f4aa0e899b8
pr-song <songtranvan2511@gmail.com>
2022-09-12 01:50:06 +0900

    init fizzbuzz

--- src/Tdd/FizzBuzz.php
+++ src/Tdd/FizzBuzz.php
@@ -1,9 +1,24 @@
 <?php
 
+namespace Tdd;
+
 class FizzBuzz
 {
-
-    public function __construct()
+    /**
+     * @param int $number
+     * @return string
+     */
+    public function convertNumberToString(int $number): string
     {
+        if ($number % 3 === 0 && $number % 5 === 0) {
+            return "FizzBuzz";
+        }
+        if ($number % 3 === 0) {
+            return "Fizz";
+        }
+        if ($number % 5 === 0) {
+            return "Buzz";
+        }
+        return (string)$number;
     }
 }
